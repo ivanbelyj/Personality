d08f8c9222791304fe28a9b39b9ce57434a7ae24
Ivan Belyj <ivanbelyj743@gmail.com>
2024-10-20 17:59:24 +0300

    Add communication ui toggle

--- Runtime/Communication/UI/CommunicationUIManager.cs
+++ Runtime/Communication/UI/CommunicationUIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,16 +15,37 @@ public class CommunicationUIManager : MonoBehaviour
 
     // [SerializeField]
     // private RectTransform panel;
+
     // [SerializeField]
     // private float animationSpeed = 1.0f;
-    // private bool isVisible = true;
+
+    [SerializeField]
+    private float transitionDuration = 0.6f;
+
+    [SerializeField]
+    private CanvasGroup[] canvasGroupsToHide;
+
+    private Coroutine panelAnimationCoroutine;
+
+    private bool IsVisible => canvasGroupsToHide[0].alpha > 0;
+
+    private void Awake() {
+        if (canvasGroupsToHide.Length == 0) {
+            Debug.LogWarning(
+                $"{nameof(canvasGroupsToHide)} is empty. Animation won't be applied"
+            );
+        }
+    }
 
     public void ToggleUI()
     {
-        Debug.Log("Toggle UI");
-        // Todo: hide all but messages
-        // isVisible = !isVisible;
-        // StartCoroutine(AnimatePanel());
+        if (panelAnimationCoroutine != null) {
+            return;
+        }
+
+        if (canvasGroupsToHide.Length > 0) {
+            panelAnimationCoroutine = StartCoroutine(AnimatePanel());
+        }
     }
 
     // private IEnumerator AnimatePanel()
@@ -41,4 +63,52 @@ public class CommunicationUIManager : MonoBehaviour
 
     //     panel.anchoredPosition = new Vector2(targetX, 0);
     // }
+
+    private IEnumerator AnimatePanel() {
+        float startAlpha = canvasGroupsToHide[0].alpha;
+        float targetAlpha = IsVisible ? 0f : 1f;
+        yield return StartCoroutine(SmoothTransition(
+            startAlpha,
+            targetAlpha,
+            transitionDuration,
+            (newValue) => {
+                foreach (var canvasGroup in canvasGroupsToHide) {
+                    canvasGroup.alpha = newValue;
+                }
+            }));
+
+        OnEndAnimation();
+    }
+
+    private void OnEndAnimation() {
+        panelAnimationCoroutine = null;
+
+        foreach (var canvasGroup in canvasGroupsToHide) {
+            canvasGroup.interactable = IsVisible;
+        }
+    }
+
+    // Todo: use from common package ?
+    private static IEnumerator SmoothTransition(
+        float startValue,
+        float endValue,
+        float duration,
+        Action<float> applyNewValue)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            t = t * t * (3f - 2f * t);
+
+            float newValue = Mathf.Lerp(startValue, endValue, t);
+            applyNewValue(newValue);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        applyNewValue(endValue);
+    }
 }
